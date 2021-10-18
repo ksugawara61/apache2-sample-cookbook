b6dc34844b2327eeb269b04360cc075b5b0aebda
ksugawara61 <ksugawara61@users.noreply.github.com>
2021-10-18 22:11:28 +0900

    add: template

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+package 'apache2' do
+  action :install
+end
+
+service 'apache2' do
+  action %i[enable start]
+end
+
+template 'apache2.conf' do
+  path '/etc/apache2/apache2.conf'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  notifies :reload, 'service[apache2]'
+end
